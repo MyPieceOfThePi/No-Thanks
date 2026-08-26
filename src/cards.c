395934e7fc31dad2635b2559d3cf65892ebae43f
Finn Jasmann <finnjasmann@gmail.com>
2026-08-26 12:30:00 -0700

    Removed brackets around cards in to_print.

--- src/cards.c
+++ src/cards.c
@@ -28,5 +28,5 @@ Card deal_card(Deck *deck){
 }
 
 int to_string(const Card card, char *str){
-    return sprintf(str, "[%s%s]", ranks[card.rank], suits[card.suit]);
+    return sprintf(str, "%s%s", ranks[card.rank], suits[card.suit]);
 }
